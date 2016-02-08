902dc0777edf7bfb64bde3bd42e8256a583f69b0
Odie Edo-Osagie <O.Edo-Osagie@uea.ac.uk>
2016-02-08 16:33:06 +0000

    x

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+/* Find cron docs here - https://www.npmjs.com/package/cron */
+var jobCounter = 0;
+
 var express = require('express');
 var app = express();
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob({
+  cronTime: '*/1 * * * *',
+  onTick: function() {
+      jobCouter++;
+      console.log("Ran job for the " + jobCounter + "th time");
+  },
+  start: false,
+});
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -10,3 +23,5 @@ app.get('/', function (req, res) {
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!');
 });
+
+job.start();
